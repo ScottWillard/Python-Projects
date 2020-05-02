aa023b6b2fc3728a29579048397c4333b0de4d54
Scot Willard programming hub <Scottwillard9698@gmail.com>
2020-05-01 22:26:23 -0400

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,97 @@
+import random
+
+option = ""
+pRCounter = 0
+pPCounter = 0
+pSCounter = 0
+cRCounter = 0
+cPCounter = 0
+cSCounter = 0
+
+
+def RPS():
+    rps = ["rock", "paper", "scissors"]
+    playerScore = 0
+    computerScore = 0
+    player = input("[Rock/Paper/Scissors] Choose 1 or PRESS 'x' TO QUIT: ")
+    computer = random.choice(rps)
+    if player == "x":
+        print("Thanks for playing!\n\n")
+        exit(0)
+    else:
+        print("Computer chose: ", computer)
+
+    if computer == player:
+        print("Draw\n")
+    elif computer == "paper" and player == "rock":
+        print("Computer wins!\n")
+        computerScore += 1
+    elif computer == "rock" and player == "scissors":
+        print("Computer wins!\n")
+        computerScore += 1
+    elif computer == "scissors" and player == "paper":
+        print("Computer wins!\n")
+        computerScore += 1
+    elif player == "paper" and computer == "rock":
+        print("You win!\n")
+        playerScore += 1
+    elif player == "rock" and computer == "scissors":
+        print("You win!\n")
+        playerScore += 1
+    elif player == "scissors" and computer == "paper":
+        print("You win!\n")
+        playerScore += 1
+
+    print("===========SCORE===========")
+    print("Player:", playerScore, "    ", end = ' ')
+    print("Computer:", computerScore)
+    print("===========================")
+    print("\n")
+    RPS()
+    if playerScore == 10 or computerScore == 10:
+        exit(0)
+
+
+def RPS2():
+    playerScore = 0
+    computerScore = 0
+    while playerScore < 15 and computerScore < 15:
+        rps = ["rock", "paper", "scissors"]
+        player = random.choice(rps)
+        computer = random.choice(rps)
+        print("Player chose: ", player)
+        print("Computer chose: ", computer)
+        if computer == player:
+            print("Draw\n")
+        elif computer == "paper" and player == "rock":
+            # print("Computer wins!\n")
+            computerScore += 1
+        elif computer == "rock" and player == "scissors":
+            # print("Computer wins!\n")
+            computerScore += 1
+        elif computer == "scissors" and player == "paper":
+            # print("Computer wins!\n")
+            computerScore += 1
+        elif player == "paper" and computer == "rock":
+            # print("You win!\n")
+            playerScore += 1
+        elif player == "rock" and computer == "scissors":
+            # print("You win!\n")
+            playerScore += 1
+        elif player == "scissors" and computer == "paper":
+            # print("You win!\n")
+            playerScore += 1
+
+        print("===========SCORE===========")
+        print("Player:", playerScore, "    ", end = ' ')
+        print("Computer:", computerScore)
+        print("===========================")
+        print("\n")
+
+
+print("1) You vs. CPU\n2) CPU vs. CPU\n")
+option = input()
+if option == '1':
+    RPS()
+elif option == '2':
+    RPS2()
