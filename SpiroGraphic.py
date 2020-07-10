bc1c96df91c27973dbe09d7fa14340f81b8b2802
Scot Willard programming hub <Scottwillard9698@gmail.com>
2020-07-10 16:42:21 -0400

    Add files via upload

---
+++ SpiroGraphic.py
@@ -0,0 +1,31 @@
+import turtle
+from turtle import *
+import numpy as np
+import random
+
+my_turtle = turtle.Turtle()
+my_turtle.speed(9999999999999999)
+colors = ["bright red", "DeepPink3", "light orange", "lime green", "cyan2", "DarkOrchid1", "bisque"]
+a = 30.14159
+
+
+def drawArt(d , angle, x, y):
+    bgcolor(random.choice(colors))
+    my_turtle.up()
+    my_turtle.goto(x, y)
+    my_turtle.down()
+    my_turtle.pencolor(random.choice(colors))
+    for i in range(1, 500):
+        while my_turtle.pencolor() == bgcolor():
+            my_turtle.pencolor(random.choice(colors))
+        my_turtle.forward(d-14.5)
+        my_turtle.left(angle)
+        d -= 1
+    turtle.clearscreen()
+
+
+while a < 360:
+    drawArt(15, a, 0, 0)
+    a += 1
+
+
